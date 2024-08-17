9a6673729ca9de09839663d6b921726227ae3860
hritik <rohan.kinshu@gmail.com>
2024-08-17 15:28:08 +0530

    ..

--- oauth/src/main/java/com/rohan/oauth2/oauth/SecurityConfig/Security.java
+++ oauth/src/main/java/com/rohan/oauth2/oauth/SecurityConfig/Security.java
@@ -1,4 +1,28 @@
 package com.rohan.oauth2.oauth.SecurityConfig;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
 public class Security {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(AbstractHttpConfigurer::disable) // Disable CSRF protection
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().authenticated() // Require authentication for any request
+                )
+                .oauth2Login(withDefaults()); // Use OAuth2 login with default configurations
+
+        return http.build();
+    }
 }
